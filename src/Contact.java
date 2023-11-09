d1b7922a02960364d4aee2b73b89d6dc3260cbd2
bryce <25107568+xSova@users.noreply.github.com>
2023-11-09 17:21:18 -0600

    added constructor to Contact and made setters

--- src/Contact.java
+++ src/Contact.java
@@ -13,11 +13,31 @@ public class Contact {
     private String lastName;
     private String phone;
     private String address;
-    private static final String id;
+    private final String contactID;
 
     // Constructor
-    public Contact(String firstName, String lastName, String phone, String address) {
+    public Contact(String contactID, String firstName, String lastName, String phone, String address) {
+        if (contactID == null || contactID.length() > maxLength) {
+            throw new IllegalArgumentException("Invalid contact ID.");
+        }
+        if (firstName == null || firstName.length() > maxLength) {
+            throw new IllegalArgumentException("Invalid first name.");
+        }
+        if (lastName == null || lastName.length() > maxLength) {
+            throw new IllegalArgumentException("Invalid last name.");
+        }
+        if (phone == null || phone.length() > maxLength) {
+            throw new IllegalArgumentException("Invalid phone number.");
+        }
+        if (address == null || address.length() > 30) {
+            throw new IllegalArgumentException("Invalid address.");
+        }
 
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.phone = phone;
+        this.address = address;
+        this.contactID = contactID;
     }
 
     // Getters
@@ -25,28 +45,42 @@ public class Contact {
     public String getFirstName() { return firstName; }
     public String getLastName() { return lastName; }
     public String getPhone() { return phone; }
-    public String getId() { return id; }
+    public String getId() { return contactID; }
 
     // Setters
+    // No setter for contactID because it will not be changed.
     public void setFirstName(String firstName) {
         if (firstName != null && firstName.length() <= maxLength) {
             this.firstName = firstName;
         } else {
-            throw new IllegalArgumentException("Error: Input invalid.");
+            throw new IllegalArgumentException("Error: Invalid first name.");
         }
     }
 
     public void setLastName(String lastName) {
-        this.lastName = lastName;
+        if (lastName != null && firstName.length() <= maxLength) {
+            this.lastName = lastName;
+        } else {
+            throw new IllegalArgumentException("Error: Invalid last name.");
+        }
     }
 
     public void setPhone(String phone) {
-        this.phone = phone;
+        if (phone != null && phone.length() <= maxLength) {
+            this.phone = phone;
+        } else {
+            throw new IllegalArgumentException("Error: Invalid phone number.");
+        }
     }
 
-    public void setId(String id) {
-        this.id = id;
+    public void setAddress(String address) {
+        if (address != null && address.length() <= 30) {
+             this.address = address;
+        } else {
+            throw new IllegalArgumentException("Error: Invalid address.");
+        }
     }
+
 
     // make something iterate and pass in requirements for max len and what is needed like phone numbers
     // loop for input until valid input is given
